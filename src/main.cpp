2c42cf7c1f26ab152b4c2995d16c2e70cfcb76d9
Jake Howard <git@theorangeone.net>
2017-01-25 09:35:09 +0000

    Add most of logic

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+
+#define READ_PIN 9
+#define KEY_TO_PRESS KEY_RETURN
+#define REPEAT_DELAY 1000
+
+bool isHigh = false;
+
+void handleInput() {
+    Serial.println('Handled!');
+    delay(REPEAT_DELAY);
+}
+
+
+void setup() {
+    pinMode(READ_PIN, INPUT_PULLUP);
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (digitalRead(READ_PIN) == HIGH && !isHigh) {
+        isHigh = true;
+        handleInput();
+    }
+    if (digitalRead(READ_PIN) == LOW && isHigh) {
+        isHigh = false;
+    }
+}
